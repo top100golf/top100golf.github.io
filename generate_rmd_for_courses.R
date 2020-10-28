a0d9806b0dbfe1c63bd434e8fd19cbfd0878c653
Skip Perry <cardinal403@outlook.com>
2020-10-28 14:14:41 -0700

    progress

---
+++ generate_rmd_for_courses.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(rmarkdown)
+
+gcr_dir <- "/Users/skipperry/Documents/golf_course_rankings/"
+web_dir <- "/Users/skipperry/Documents/top100golf.github.io/courses/"
+
+source(str_c(gcr_dir, "plot_rankings.R"))
+
+course_list <- 
+  all_rankings %>% 
+  filter(Year == 2019) %>% 
+  select(Course) %>% 
+  distinct() %>% 
+  pull()
+
+# individual courses to re-run
+# course_list <- c("", "")
+
+for (course in course_list) {
+  readLines("/Users/skipperry/Documents/golf_course_rankings/single_course.Rmd") %>% 
+    writeLines(str_c("/Users/skipperry/Documents/top100golf.github.io/", course, ".Rmd"))
+}
+
+
