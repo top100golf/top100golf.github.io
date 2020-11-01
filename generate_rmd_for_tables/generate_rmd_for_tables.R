5293d3ba56da0b92cd64aef9caf6677fe861ca57
Skip Perry <cardinal403@outlook.com>
2020-11-01 11:13:34 -0800

    progress

---
+++ generate_rmd_for_tables/generate_rmd_for_tables.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(rmarkdown)
+
+table_dir <- "/Users/skipperry/Documents/top100golf.github.io/generate_rmd_for_tables/"
+web_dir <- "/Users/skipperry/Documents/top100golf.github.io/"
+
+for (year1 in seq(1993, 2019, by = 2)) {
+  year2 <- year1 + 1
+  readLines(str_c(table_dir, "rankings_year1_year2.Rmd")) %>% 
+    writeLines(str_c(web_dir, "rankings_", year1, "_", year2, ".Rmd"))
+}
+
+
