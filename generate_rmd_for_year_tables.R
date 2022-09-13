3adc1d220a365de6856987d003c9ead14c6c1fe6
Skip Perry <cardinal403@outlook.com>
2022-09-12 20:02:45 -0700

    progress

---
+++ generate_rmd_for_year_tables.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(rmarkdown)
+
+gcr_dir <- "/Users/skipperry/Documents/golf_course_rankings/"
+web_dir <- "/Users/skipperry/Documents/top100golf.github.io/courses/"
+
+year_list <- seq(1991, 2019, by = 2)
+
+for (year in year_list) {
+  readLines("/Users/skipperry/Documents/golf_course_rankings/rankings_year1_year2.Rmd") %>% 
+    writeLines(str_c("/Users/skipperry/Documents/top100golf.github.io/rankings_", year, "_", year + 1, ".Rmd"))
+}
+
+
