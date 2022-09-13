08653bb76f65b2dfab7b14e131f29c2c43a8b09e
Skip Perry <cardinal403@outlook.com>
2022-09-12 19:38:07 -0700

    progress

--- generate_rmd_for_courses.R
+++ generate_rmd_for_courses.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(rmarkdown)
+library(gridExtra)
 
 gcr_dir <- "/Users/skipperry/Documents/golf_course_rankings/"
 web_dir <- "/Users/skipperry/Documents/top100golf.github.io/courses/"
@@ -11,6 +12,7 @@ course_list <-
   all_rankings %>% 
   select(Course) %>% 
   distinct() %>% 
+  mutate(Course = str_replace_all(Course, "\\s", "\\-")) %>% 
   pull()
 
 # individual courses to re-run
@@ -20,7 +22,8 @@ course_list <-
 
 # course_list <- ex_top_100
 
-course_list <- "Milwaukee"
+#course_list <-
+#  course_list[1:2]
 
 for (course in course_list) {
   readLines("/Users/skipperry/Documents/golf_course_rankings/single_course.Rmd") %>% 
