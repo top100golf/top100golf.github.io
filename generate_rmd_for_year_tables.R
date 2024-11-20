5be3b524f8da8db0375ee374ba7b2139f86d42af
Skip Perry <cardinal403@outlook.com>
2024-11-20 13:24:12 -0800

    2023 golf mag main update

--- generate_rmd_for_year_tables.R
+++ generate_rmd_for_year_tables.R
@@ -5,8 +5,8 @@ gcr_dir <- "/Users/skipperry/Documents/golf_course_rankings/"
 web_dir <- "/Users/skipperry/Documents/top100golf.github.io/courses/"
 
 #year_list <- seq(2019, 2021, by = 2)
-year_list <- seq(1991, 2021, by = 2)
-# Once Golf Mag 2023 is out, change to 2023
+year_list <- seq(1991, 2023, by = 2)
+# Once Golf Mag 2024 is out, change to 2025 and make new HTML file for old rankings
 
 for (year in year_list) {
   readLines("/Users/skipperry/Documents/golf_course_rankings/rankings_year1_year2.Rmd") %>% 
